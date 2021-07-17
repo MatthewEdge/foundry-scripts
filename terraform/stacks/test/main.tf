3b1c596c689c6fa2d4568b3621030a12cf0d5e37
Frank Kerschbaumer <FrankKerschbaumer3@gmail.com>
2021-07-17 18:54:29 -0400

    Terraform Instance Creation (#1)
    * Fix vars for instance creation. Auto-create EBS volume in favor of S3 data sync
    
    Co-authored-by: Matthew Edge <medge@medgelabs.io>

---
+++ terraform/stacks/test/main.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "linux2_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "foundry_instance" {
+  ami                    = data.aws_ami.linux2_ami.id
+  availability_zone      = "${var.region}a"
+  key_name               = "findthepath"
+  instance_type          = "c5.xlarge"
+  subnet_id              = "subnet-0627ae7cbbe84f6d9"
+  vpc_security_group_ids = ["sg-069b1d42ccfb9a3d3"]
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 30
+  }
+
+  tags = {
+    Name = "${var.tag}-ec2-instance"
+    App  = var.tag
+  }
+}
---
+++ terraform/stacks/test/variables.tf
@@ -0,0 +1,8 @@
+variable "tag" {
+  default     = "FoundryVTT"
+  description = "foundry tag name"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
