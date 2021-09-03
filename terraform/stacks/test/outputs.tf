df6385ed5b92909ba115252266b0b754c5ff6f99
frankkerschbaumer3 <frankkerschbaumer3@gmail.com>
2021-09-03 16:24:20 -0400

    Add lambda outputs

--- terraform/stacks/test/outputs.tf
+++ terraform/stacks/test/outputs.tf
@@ -6,3 +6,11 @@ output "instance_ip_addr" {
 output "instance_key_name" {
   value = aws_instance.foundry_instance.key_name
 }
+
+output "scheduler_lambda_arn" {
+  value = aws_lambda_function.scheduler_lambda.arn
+}
+
+output "scheduler_lambda_function_name" {
+  value = aws_lambda_function.scheduler_lambda.function_name
+}
