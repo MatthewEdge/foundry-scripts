38005d6ec689eb34e42e07724132fb1bf5bde1a4
frankkerschbaumer3 <frankkerschbaumer3@gmail.com>
2021-09-03 17:05:17 -0400

    Fix cron expression

--- terraform/stacks/test/variables.tf
+++ terraform/stacks/test/variables.tf
@@ -16,7 +16,7 @@ variable "bucket" {
 }
 
 variable "schedule_expression" {
-  default     = "cron(0 5 * * *)"
+  default     = "cron(0 5 * * ? *)"
   description = "the aws cloudwatch event rule schedule expression that specifies when the scheduler runs. Default is 5 minuts past the hour. for debugging use 'rate(5 minutes)'. See https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html"
 }
 
