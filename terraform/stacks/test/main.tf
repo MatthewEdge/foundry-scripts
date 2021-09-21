65170623e12f4163b8bb6dce5c56c2edd60dbf55
frankkerschbaumer3 <frankkerschbaumer3@gmail.com>
2021-09-20 20:10:59 -0400

    Move outputs to outputs file

--- terraform/stacks/test/main.tf
+++ terraform/stacks/test/main.tf
@@ -89,11 +89,3 @@ resource "aws_route53_record" "foundry" {
   ttl = "300"
   records = [aws_instance.foundry_instance.public_ip]
 }
-
-output "instance_ip_addr" {
-  value = aws_instance.foundry_instance.public_ip
-}
-
-output "instance_key_name" {
-  value = aws_instance.foundry_instance.key_name
-}
