28eb2dcc748807a1bc11e5205dde580bfe14fe8b
frankkerschbaumer3 <frankkerschbaumer3@gmail.com>
2022-03-23 20:22:29 -0400

    Add script to run the ami build

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+START_DIR=$PWD
+cd packer/
+
+export ACCESS_KEY=$(cat $HOME/.aws/credentials| grep aws_access_key_id | cut -d '=' -f2 | cut -d ' ' -f2)
+export SECRET_KEY=$(cat $HOME/.aws/credentials| grep aws_secret_access_key | cut -d '=' -f2 | cut -d ' ' -f2)
+
+echo "Creating ami..."
+docker run --rm -e AWS_ACCESS_KEY_ID="${ACCESS_KEY}" -e AWS_SECRET_ACCESS_KEY="${SECRET_KEY}" -v $PWD:/src -w /src hashicorp/packer:light init
+docker run --rm -e AWS_ACCESS_KEY_ID="${ACCESS_KEY}" -e AWS_SECRET_ACCESS_KEY="${SECRET_KEY}" -v $PWD:/src -w /src hashicorp/packer:light validate
+docker run --rm -e AWS_ACCESS_KEY_ID="${ACCESS_KEY}" -e AWS_SECRET_ACCESS_KEY="${SECRET_KEY}" -v $PWD:/src -w /src hashicorp/packer:light apply -auto-approve
